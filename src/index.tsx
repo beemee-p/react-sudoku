94a7a9182a1cf180c331902ef86b68fec50354f3
yunapark <raina.y.park@gmail.com>
2022-02-08 21:45:20 +0900

    Add border to 3x3 blocks

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,10 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Content data-cy="content">
-    <Title data-cy="title">Sudoku</Title>
-    <Card data-cy="card">
-      <Grid />
-    </Card>
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
