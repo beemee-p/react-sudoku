66bfa402e56afc90b1c3b2bde19aa9bacf92c3fb
Yuna Park <una@stunning.kr>
2022-03-13 21:23:58 +0900

    Test fillgrid func

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,10 +1,22 @@
 import React, { FC, Children } from 'react'
+import { GRID } from 'typings'
+import { fillGrid } from 'utils'
 import Block from './block'
 import { Container, Row } from './styles'
 
 const Grid: FC = () => {
-  // other
-
+  const grid: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+  fillGrid(grid)
   return (
     <Container data-cy="grid-container">
       {Children.toArray(
